75ba225c218c782c1086c67c4bd51680767a3105
Prabakaran <r.prabakaran99@gmail.com>
2025-10-13 20:18:16 +0530

    arrays

---
+++ src/main/java/org/dsa/array/ExcelColumnTitle.java
@@ -0,0 +1,19 @@
+package org.dsa.array;
+
+public class ExcelColumnTitle {
+
+    public String convertToTitle(int A) {
+
+        StringBuilder sb = new StringBuilder();
+
+        while(A>0){
+            int rem = (A-1) % 26;
+            sb.append((char) (rem + 'A'));
+            A = (A-1)/26;
+
+        }
+        return sb.reverse().toString();
+
+    }
+}
+
